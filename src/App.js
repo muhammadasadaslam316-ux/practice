c4a868ead20f0977aa93b15c8e683d3e95aac16a
Asad.Learning <muhammadasadaslam316@gmail.com>
2026-07-20 08:38:09 -0700

    add new footer for practice

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ import Container from './container.js';
 // }
 // export default App;
 
+
+ 
+
+
 class Navbar extends React.Component {
   render() { 
     return (
+      
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
---
+++ src/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>Daraz International</p>
+        <button>Contact Us</button>
+        <button>About Us</button>
+        <button>Privacy Policy</button>
+        <button>Terms & Policy</button>
+      </div>
+    );
+  }
+}
+
+export default Footer;
